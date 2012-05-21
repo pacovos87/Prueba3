0ca9dd05449169b63ee86104f0d1e50e88a189df
fjcarmona <fjcarmona@106-15.info2>
2012-05-21 10:40:28 +0200

    Modificacion3 X1

--- src/x1.java
+++ src/x1.java
@@ -10,6 +10,10 @@ public class x1 {
 		System.out.println("2");
 		System.out.println("2");
 		System.out.println("2");
+		
+		System.out.println("3");
+		System.out.println("3");
+		System.out.println("3");
 	}
 
 }
