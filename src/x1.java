2a7d6e0abc8c2920984732e9725cbda2e967cd1c
fjcarmona <fjcarmona@106-15.info2>
2012-05-21 10:39:42 +0200

    Modificacion2 X1

--- src/x1.java
+++ src/x1.java
@@ -6,6 +6,10 @@ public class x1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("1");
+		
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
 	}
 
 }
